613eaec5bfda3c65c6e8153a0b36a22902ca6b7d
Qristine <qiskhachatryan05@gmail.com>
2024-04-01 12:55:46 +0400

    index.html finished

--- client/components/main/header_footer.js
+++ client/components/main/header_footer.js
@@ -48,7 +48,7 @@ function createMenu() {
     menuContainer.appendChild(menuItemsContainer);
 
     // Toggle menu items on mobile
-    menuToggle.addEventListener('click', function() {
+    menuToggle.addEventListener('click', function () {
         menuItemsContainer.classList.toggle('active');
     });
 }
@@ -64,8 +64,68 @@ function setActiveMenuItem(itemId) {
         }
     });
 }
+function createFooter() {
+    const footerContainer = document.getElementById('footerContainer');
+    let divgeneral = document.createElement('div');
+    divgeneral.classList.add('divgeneral');
+
+    let div_1 = document.createElement('div');
+    div_1.classList.add('div_1');
+    // Create the map image element
+    const map = document.createElement('img');
+    map.src = '../image/Location.png';
+    map.alt = 'Map';
+    div_1.appendChild(map);
+
+    // Create the paragraph element for the location
+    const location = document.createElement('p');
+    location.textContent = "Tigran Mets 7/2";
+    location.classList.add('textcolor');
+    div_1.appendChild(location);
+    divgeneral.appendChild(div_1);
+
+    let div_2 = document.createElement('div')
+    div_2.classList.add('div_2');
+    // Create the call image element
+    const call = document.createElement('img');
+    call.src = '../image/Call.png'; // Fix the typo here
+    call.alt = 'Call';
+    div_2.appendChild(call);
+
+    // Create the paragraph element for the phone number
+    const phoneNumber = document.createElement('p'); // Fix the variable name here
+    phoneNumber.textContent = "37499102030";
+    phoneNumber.classList.add('textcolor');
+    div_2.appendChild(phoneNumber);
+    divgeneral.appendChild(div_2);
+
+    let div_3 = document.createElement('div');
+    div_3.classList.add('div_3');
+    div_3.classList.add('container');
+    let instagram = document.createElement('img');
+    instagram.src = '../image/Instagram.png';
+    instagram.alt = 'Instagram';
+    let tiktok = document.createElement('img');
+    tiktok.src = '../image/TikTok.png';
+    tiktok.alt = 'TikTok'; // Corrected the alt attribute
+    let facebook = document.createElement('img');
+    facebook.src = '../image/Facebook.png'; // Added a slash before 'image'
+    facebook.alt = 'Facebook';
+    div_3.appendChild(instagram);
+    div_3.appendChild(tiktok);
+    div_3.appendChild(facebook);
+    footerContainer.appendChild(div_3);
+    footerContainer.appendChild(divgeneral);
+
+
+}
+
+
 
 // Call createMenu() function to initialize the menu
 if (document.getElementById("menuContainer")) {
     createMenu();
+}
+if (document.getElementById("footerContainer")) {
+    createFooter();
 }
